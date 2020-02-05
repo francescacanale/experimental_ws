f5b2eff8b9a8536fee803faa9289d00a9874c150
root <francesca.canale.95@gmail.com>
2020-02-05 16:54:17 +0000

    Not working publisher

--- src/ass_2/src/example_ball.py
+++ src/ass_2/src/example_ball.py
@@ -2,6 +2,8 @@
 
 # Python libs
 import sys, time
+from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import String
 
 # numpy and scipy
 import numpy as np
@@ -21,17 +23,19 @@ from sensor_msgs.msg import CompressedImage
 
 VERBOSE=False
 
-
 class image_feature:
 
     def __init__(self):
         '''Initialize ros publisher, ros subscriber'''
 
+	# Topic where we publish the translation vector
+	self.translation_pub = rospy.Publisher("translation", Float32MultiArray, queue_size=10) 
+
+	# Topic where we publish a string
+	self.pub = rospy.Publisher("chatter", String, queue_size=10) 
+
         # Topic where we publish
         self.image_pub = rospy.Publisher("/output/image_raw/compressed", CompressedImage, queue_size=1)
-
-	# Topic where we publish the translation vector
-	# self.translation_pub = rospy.Publisher("/Translation", float32, queue_size=1) # devo creare custom message
 
         # Subscribed Topic
         self.subscriber = rospy.Subscriber("/raspicam_node/image/compressed", CompressedImage, self.callback,  queue_size = 1)
@@ -67,6 +71,12 @@ class image_feature:
 		cv2.CHAIN_APPROX_SIMPLE)
 	cnts = imutils.grab_contours(cnts)
 	center = None
+	translation = Float32MultiArray()
+
+	# Publishing a string
+	stringa='ciao'
+	self.pub.publish(stringa)
+
 
 	# Only proceed if at least one contour was found
 	if len(cnts) > 0:
@@ -100,14 +110,26 @@ class image_feature:
 			# Calling solvePnP, it computes T between the object frame and the camera
 			(_, rotation_vector, translation_vector) = cv2.solvePnP(object_point, image_point , self.camera_matrix, self.dist_coefs)
 			
+			translation.layout = 'objectPosition'
+			translation.data = translation_vector
+
 			# Printing translation vector
 			print '\nTranslation vector: '
-			print translation_vector
+			print translation
+
+			# Publishing the translation vector
+			self.translation_pub.publish(translation)
+
+
  
 	# update the points queue
 	#pts.appendleft(center)
         cv2.imshow('window', image_np)
         cv2.waitKey(2)
+
+
+
+
 
      
         #self.subscriber.unregister()
