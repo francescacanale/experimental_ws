bae543f814e4720f5e609ca012a5c0485cb6ef45
root <francesca.canale.95@gmail.com>
2020-02-07 08:30:25 +0000

    Uploaded simulation in gazebo

--- src/ass_2/src/controller_node.cpp
+++ src/ass_2/src/controller_node.cpp
@@ -1,8 +1,10 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "nav_msgs/Odometry.h"
-#include "ass_1/GetPosition.h"
+#include "gazebo_msgs/ModelStates.h"
+//#include "ass_1/GetPosition.h"
 #include <std_msgs/Int32.h>
+#include <tf/transform_listener.h>
 
 #include <iostream>
 #include <fstream>
@@ -14,100 +16,102 @@
 using namespace std;
 
 ros::Publisher pub;
-ros::ServiceClient client;
+ros::Subscriber positions_sub;
+//ros::ServiceClient client;
 
-bool arrived = true; //Variable for knowing if the robot is arrived to the target position
-bool completed = false; //Variable for kwnowing when the whole map has been visited
-int *goal= new int [4]; //Variable to store the target positions and the velocities
+// Needed only in the real world:
+//ros::Subscriber sub_ball;
+//ros::Subscriber sub_robot;
 
-
-void completedCallback(const std_msgs::Int32::ConstPtr& msg)
- {
-	int a = msg->data; 
-	if(a == 1) {
-		cout<<"***Game over***\n";
-		completed = true;
-
-		//When the game is over the robots are stopped
-		geometry_msgs::Twist vel3;
-		vel3.linear.x = 0;
-		vel3.linear.y = 0;
-		vel3.angular.z = 10; //Little drift
-		pub.publish(vel3);
-	}		   
-}
+bool object_detected = false;
+bool aligned = false;
+double robot_roll, robot_pitch, robot_yaw;
+float yaw;
 
 
-// Function to compute the euclidean distance between the actual position of the robot and the target position
-float euclideanDistance(double actual_x, double actual_y, int goal_x, int goal_y)
-   {
-	return sqrt(pow((goal_x - actual_x),2) + pow((goal_y - actual_y), 2));
+float *ball= new float [2]; //Variable to store the ball position
+float *porta= new float [2]; //Variable to store the porta position
+float *robot= new float [2]; //Variable to store the robot position
+ 
+
+// Function to align the robot
+void alignment(float yaw, double robot_yaw) {
+	cout<<"SONO NELLA FUNZIONE";
+	
+	float misalignment = yaw - robot_yaw;
+
+	if(abs(misalignment) < (M_PI - 0.01) && abs(misalignment) > 0.01) {
+		geometry_msgs::Twist vel;
+		vel.angular.z = 0.5 * misalignment;
+		pub.publish(vel);
+		cout<<"FACCIO GIRAREEEEEEE";
+	}
+
+	else
+		aligned = true;
 }
 
 
 // Callback for the odometry messages
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
-   {
-	
-	//Odometry of the robot
-    double x_current = msg->pose.pose.position.x;
-    double y_current = msg->pose.pose.position.y;
+   {	
+	//Computing pose robot
+    robot[0] = msg->pose.pose.position.x;
+    robot[1] = msg->pose.pose.position.y;
 
-	if (arrived == true) {
-		
-		arrived = false;
+	// Computing orientation robot
+	tf::Quaternion q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+	tf::Matrix3x3 m(q);
+	m.getRPY(robot_roll, robot_pitch, robot_yaw);
+	cout<<"Yaw robot = ";
+	cout<<robot_yaw;
+	cout<<"\n";
 
-		//Calling the service to know the target position and the direction of the velocity
-		ass_1::GetPosition srv;
-		srv.request.my_position_x = x_current;
-		srv.request.my_position_y = y_current;
-		client.call(srv);
-
-		goal[0] = srv.response.target_x;
-		goal[1] = srv.response.target_y;
-		goal[2] = srv.response.vel_x;
-		goal[3] = srv.response.vel_y;
+	if (aligned == false) {
+		// Calling the function to align the robot
+		alignment(yaw, robot_yaw);
 	}
-
-	float distance = euclideanDistance(x_current, y_current, goal[0], goal[1]);
-	float distance_tolerance = 0.1;
-
-	//If I'm not arrived to the target position yet, I give the velocities to the robot
-	if(distance >= distance_tolerance && completed == false) { 
-		geometry_msgs::Twist vel;
-        vel.linear.x = 0.5 * goal[2];
-		vel.linear.y = 0.5 * goal[3];
-        pub.publish(vel);
-	}
-	//If I'm arrived to the traget position the robot stops
-	else { 
-		arrived = true; 
-		geometry_msgs::Twist vel2;
-		vel2.linear.x = 0;
-		vel2.linear.y = 0;
-		pub.publish(vel2);
+	else {
+		cout<<"I'm alinged";
+        // Parrtire dritto
 	}
 }
-	
 
+// Callback for the model objects
+void modelCallback(const gazebo_msgs::ModelStates::ConstPtr& msg)
+   {
+	//Position of the ball
+    ball[0] = msg->pose[1].position.x;
+    ball[1] = msg->pose[1].position.y;
+
+	//Position of the porta
+    porta[0] = msg->pose[2].position.x;
+    porta[1] = msg->pose[2].position.y;
+
+	object_detected = true;
+
+	if(object_detected) {
+		float m = (porta[1]-ball[1])/(porta[0]-ball[0]); //Angular coefficent of the misalignment
+		yaw = atan(m); //Yaw angle wrt y axis of the world frame
+		cout<<"Yaw = ";
+		cout<<yaw;
+		cout<<"\n";
+	}
+}	
+	
 
 int main(int argc, char **argv)
 {
-	//Initializing the vector for the target positions and for the velocities
-   	goal[0] = 0;
-	goal[1] = 0;
-	goal[2] = 0;
-	goal[3] = 0;
-
 	ros::init(argc, argv, "controller");
 	ros::NodeHandle n;
-	pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 5); //Publisher of the velocities
 
-	//Initializing the service client
-	client = n.serviceClient<ass_1::GetPosition>("/goal_position"); //Client of the server
+	// Publisher of the velocities
+	pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 5); 
 
-	ros::Subscriber sub_completed = n.subscribe("/completed", 5, completedCallback); //Subscriber to completed topic
-	ros::Subscriber sub = n.subscribe("odom",1000, odomCallback); //Subcriber to the odometry
+	//Subscribing to the position topic
+	ros::Subscriber robot_sub = n.subscribe("odom", 1000, odomCallback); //Subcriber to the odometry of the robot
+	ros::Subscriber moodel_sub = n.subscribe("/gazebo/model_states", 1000, modelCallback); //Subcriber to the position of the objects
+
 	ros::Rate loop_rate(10);
 	ros::spin();
 	return 0;
