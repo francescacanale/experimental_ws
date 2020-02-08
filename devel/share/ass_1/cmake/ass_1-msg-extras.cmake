942e284f1df9367654a8bdaa9ad700d4b0fe8ff4
root <francesca.canale.95@gmail.com>
2020-02-08 17:48:40 +0000

    Now it works in all the cases

--- devel/share/ass_1/cmake/ass_1-msg-extras.cmake
+++ devel/share/ass_1/cmake/ass_1-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(ass_1_MESSAGE_FILES "")
-set(ass_1_SERVICE_FILES "/home/filippo/experimental_ws/src/ass_1/srv/GetPosition.srv")
+set(ass_1_SERVICE_FILES "/home/experimental_ws/src/ass_1/srv/GetPosition.srv")
--- devel/share/ass_2/cmake/ass_2-msg-extras.cmake
+++ devel/share/ass_2/cmake/ass_2-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(ass_2_MESSAGE_FILES "/home/filippo/experimental_ws/src/ass_2/msg/Translation.msg")
+set(ass_2_MESSAGE_FILES "/home/experimental_ws/src/ass_2/msg/Translation.msg")
 set(ass_2_SERVICE_FILES "")
