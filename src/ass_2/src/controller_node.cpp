166035bc2465437ace314b02b940f2f21ed21062
root <francesca.canale.95@gmail.com>
2020-02-26 10:16:35 +0000

    Changed identation

--- src/ass_2/src/controller_node.cpp
+++ src/ass_2/src/controller_node.cpp
@@ -26,13 +26,13 @@ bool onTrajectory = false;
 bool kicked = false;
 
 float yaw, q, m;
-geometry_msgs::Twist tmp; 					// Temporary variable for comparing cmd_vel messages
-int sign_v = 0;			  					// Sign of the velocities
+geometry_msgs::Twist tmp; 			// Temporary variable for comparing cmd_vel messages
+int sign_v = 0;			  		// Sign of the velocities
 
-float *ball = new float[2];					// Variable to store the ball initial position
-float *moving_ball = new float[2];			// Variable to store the moving ball position
-float *porta = new float[2];				// Variable to store the porta initial position
-float *robot = new float[2];				// Variable to store the robot position
+float *ball = new float[2];			// Variable to store the ball initial position
+float *moving_ball = new float[2];		// Variable to store the moving ball position
+float *porta = new float[2];			// Variable to store the porta initial position
+float *robot = new float[2];			// Variable to store the robot position
 double robot_roll, robot_pitch, robot_yaw;	// Variables to store the robot orientation
 
 
@@ -58,18 +58,18 @@ void alignment(float yaw, double robot_yaw) {
 
 	// Understand the fastest way to align
 	if ((robot_yaw > yaw + 0.05 || robot_yaw < yaw + M_PI - 0.05) && abs(misalignment) > 0.05) {
-		cout << "CLOCKWISE\n"; 										// Moving the robot clockwise
+		cout << "CLOCKWISE\n"; 						// Moving the robot clockwise
 		geometry_msgs::Twist vel;
 		vel.angular.z = - 0.3 * misalignment / abs(misalignment);	// Giving an angular velocity on z axis
-		pub.publish(vel);											// Publishing the velocity
+		pub.publish(vel);						// Publishing the velocity
 		if (controlVelocities(vel))
 			pub_robot.publish(vel);
 	}
 	else if ((robot_yaw > yaw + M_PI + 0.05 || robot_yaw < yaw - 0.05) && abs(misalignment) > 0.05) {
-		cout << "ANTICLOCKWISE\n";									// Moving the robot anti-clockwise
+		cout << "ANTICLOCKWISE\n";					// Moving the robot anti-clockwise
 		geometry_msgs::Twist vel;
 		vel.angular.z = + 0.3 * misalignment / abs(misalignment);	// Giving an angular velocity on z axis
-		pub.publish(vel);											// Publishing the velocity
+		pub.publish(vel);						// Publishing the velocity
 		if (controlVelocities(vel))
 			pub_robot.publish(vel);
 	}
@@ -88,23 +88,23 @@ void alignment(float yaw, double robot_yaw) {
 // Function to move the robot back if it is not behind the ball
 void moveBack() {
 
-	float p = -m * robot[0] + robot[1];						 // y coordinate at the origin of the line of the aligned robot
-	float distance_rette = abs(p - q) / sqrt(pow(m, 2) + 1); // Distance btw two parallel lines
-	float y_robot_new;										 // y coordinate that the robot will have after the translation
+	float p = -m * robot[0] + robot[1];				// y coordinate at the origin of the line of the aligned robot
+	float distance_rette = abs(p - q) / sqrt(pow(m, 2) + 1);	// Distance btw two parallel lines
+	float y_robot_new;						// y coordinate that the robot will have after the translation
 
 	// Case 1: Parallel vertical lines
 	if (abs(q) > 100) {
-		y_robot_new = robot[1];				// If the lines are vertical the y of the robot remains the same
+		y_robot_new = robot[1];			// If the lines are vertical the y of the robot remains the same
 		if (ball[1] < y_robot_new + 1) { 	// If the robot after the translation will not be behind the ball
 			cout << "MOVING BACK\n";        // Moving the robot back
 			geometry_msgs::Twist vel;
-			vel.linear.x = -0.2;			// Giving a linear velocity on x axis
-			pub.publish(vel);				// Publishing the velocity
+			vel.linear.x = -0.2;		// Giving a linear velocity on x axis
+			pub.publish(vel);		// Publishing the velocity
 			if (controlVelocities(vel))
 				pub_robot.publish(vel);
 		}
 		else {
-			behindBall = true;				// The robot is now behind the ball
+			behindBall = true;		// The robot is now behind the ball
 			cout << "Behind the ball\n";
 		}
 	}
@@ -121,13 +121,13 @@ void moveBack() {
 		if (ball[1] < y_robot_new + 1) { 	// If the robot after the translation will not be behind the ball
 			cout << "MOVING BACK\n";        // Moving the robot back
 			geometry_msgs::Twist vel;
-			vel.linear.x = -0.2;			// Giving a linear velocity on x axis
-			pub.publish(vel);				// Publishing the velocity
+			vel.linear.x = -0.2;		// Giving a linear velocity on x axis
+			pub.publish(vel);		// Publishing the velocity
 			if (controlVelocities(vel))
 				pub_robot.publish(vel);
 		}
 		else {
-			behindBall = true;				// The robot is now behind the ball
+			behindBall = true;		// The robot is now behind the ball
 			cout << "Behind the ball\n";
 		}
 	}
@@ -144,13 +144,13 @@ void moveBack() {
 		if (ball[1] < y_robot_new + 1) {	// If the robot after the translation will not be behind the ball
 			cout << "MOVING BACK\n";        // Moving the robot back
 			geometry_msgs::Twist vel;
-			vel.linear.x = -0.2;			// Giving a linear velocity on x axis
-			pub.publish(vel);				// Publishing the velocity
+			vel.linear.x = -0.2;		// Giving a linear velocity on x axis
+			pub.publish(vel);		// Publishing the velocity
 			if (controlVelocities(vel))
 				pub_robot.publish(vel);
 		}
 		else {
-			behindBall = true;				// The robot is now behind the ball
+			behindBall = true;		// The robot is now behind the ball
 			cout << "Behind the ball\n";
 		}
 	}
@@ -160,30 +160,30 @@ void moveBack() {
 // Function to move the robot on the trajectory ball-porta
 void moveToTrajectory() {
 
-	float p = -m * robot[0] + robot[1];							// y coordinate at the origin of the line of the aligned robot
+	float p = -m * robot[0] + robot[1];				// y coordinate at the origin of the line of the aligned robot
 	float distance_rette = abs(p - q) / sqrt(pow(m, 2) + 1);	// Distance btw to parallel lines
-	float distance_rette_vertical = robot[0] - ball[0];			// Distance btw two parallel lines, vertical case
+	float distance_rette_vertical = robot[0] - ball[0];		// Distance btw two parallel lines, vertical case
 
 	
-	if (abs(q) > 100 && abs(distance_rette_vertical) > 0.1) { 							// Particular case of vertical lines
-		cout << "TRASLATION\n";															// Moving the robot on the trajectory ball-porta
+	if (abs(q) > 100 && abs(distance_rette_vertical) > 0.1) { 	// Particular case of vertical lines
+		cout << "TRASLATION\n";					// Moving the robot on the trajectory ball-porta
 		geometry_msgs::Twist vel;
 		vel.linear.y = 0.2 * distance_rette_vertical / abs(distance_rette_vertical); 	// Giving a linear velocity on y axis
-		pub.publish(vel);																// Publishing the velocity
+		pub.publish(vel);					// Publishing the velocity
 		if (controlVelocities(vel))
 			pub_robot.publish(vel);
 
 	}
-	else if (abs(distance_rette) > 0.1) {	// Normal case of oblique lines
-		cout << "TRASLATION\n";				// Moving the robot on the trajectory ball-porta
+	else if (abs(distance_rette) > 0.1) {		// Normal case of oblique lines
+		cout << "TRASLATION\n";			// Moving the robot on the trajectory ball-porta
 		geometry_msgs::Twist vel;
 		vel.linear.y = 0.2 * sign_v;		// Giving a linear velocity on y axis
-		pub.publish(vel);					// Publishing the velocity
+		pub.publish(vel);			// Publishing the velocity
 		if (controlVelocities(vel))
 			pub_robot.publish(vel);
 	}
 	else {
-		onTrajectory = true;		// The robot is now on the trajectory ball-porta
+		onTrajectory = true;			// The robot is now on the trajectory ball-porta
 		cout << "I've traslated\n";						
 		geometry_msgs::Twist vel;
 		vel.linear.y = 0;			// Giving a zero linear velocity on y axis
@@ -198,10 +198,10 @@ void moveToTrajectory() {
 void kick() {
 
 	if (moving_ball[0] == ball[0] && moving_ball[1] == ball[1]) {	// If the ball is still
-		cout << "GO FORWARD\n";										// Moving the robot foward to kick the ball
+		cout << "GO FORWARD\n";					// Moving the robot foward to kick the ball
 		geometry_msgs::Twist vel;
-		vel.linear.x = 0.3;											// Giving a linear velocity on x axis
-		pub.publish(vel);											// Publishing the velocity
+		vel.linear.x = 0.3;					// Giving a linear velocity on x axis
+		pub.publish(vel);					// Publishing the velocity
 		if (controlVelocities(vel))
 			pub_robot.publish(vel);
 	}
@@ -271,9 +271,9 @@ void modelCallback(const gazebo_msgs::ModelStates::ConstPtr &msg) {
 	porta[0] = msg->pose[2].position.x;
 	porta[1] = msg->pose[2].position.y;
 
-	m = (porta[1] - ball[1]) / (porta[0] - ball[0]);						// Angular coefficent of the line ball-porta
+	m = (porta[1] - ball[1]) / (porta[0] - ball[0]);			// Angular coefficent of the line ball-porta
 	q = (porta[0] * ball[1] - ball[0] * porta[1]) / (porta[0] - ball[0]);	// y coordinates at the origin of the line ball-porta
-	yaw = atan(m);															// Yaw angle wrt x axis of the world frame
+	yaw = atan(m);								// Yaw angle wrt x axis of the world frame
 }
 
 
@@ -293,12 +293,12 @@ int main(int argc, char **argv) {
 	while (1) {
 
 		// Defining the Subrscribers
-		ros::Subscriber robot_sub = n.subscribe("odom", 1000, odomCallback);					// Subcriber to the odometry of the robot
+		ros::Subscriber robot_sub = n.subscribe("odom", 1000, odomCallback);			// Subcriber to the odometry of the robot
 		ros::Subscriber moodel_sub = n.subscribe("/gazebo/model_states", 1000, modelCallback);	// Subcriber to the initial position of the objects
-		ros::Subscriber link_sub = n.subscribe("/gazebo/link_states", 1000, linkCallback);		// Subcriber to the moving position of the ball
+		ros::Subscriber link_sub = n.subscribe("/gazebo/link_states", 1000, linkCallback);	// Subcriber to the moving position of the ball
 
 		// Defining the Publishers
-		pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 100);				// Publisher of the velocities for Gazebo
+		pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 100);		// Publisher of the velocities for Gazebo
 		pub_robot = n.advertise<geometry_msgs::Twist>("robot/cmd_vel", 100);	// Publisher of the velocities for Arduino
 
 		ros::spin();
--- src/ass_2/src/controller_node_arena.cpp
+++ src/ass_2/src/controller_node_arena.cpp
@@ -25,12 +25,12 @@ bool onTrajectory = false;
 bool kicked = false;
 
 float yaw, q, m;
-geometry_msgs::Twist tmp; 					// Temporary variable for comparing cmd_vel messages
-int sign_v = 0;			  					// Sign of the velocities
+geometry_msgs::Twist tmp; 			// Temporary variable for comparing cmd_vel messages
+int sign_v = 0;			  		// Sign of the velocities
 
-float *ball = new float[4];					// Variable to store the ball initial position
-float *porta = new float[2];				// Variable to store the porta initial position
-float *robot = new float[3];				// Variable to store the robot position
+float *ball = new float[4];			// Variable to store the ball initial position
+float *porta = new float[2];			// Variable to store the porta initial position
+float *robot = new float[3];			// Variable to store the robot position
 double robot_roll, robot_pitch, robot_yaw;	// Variables to store the robot orientation
 
 
@@ -53,11 +53,11 @@ void search() {
 
 	// If the ball has not been detected yet
 	if (detected = false) {
-		cout << "SEARCHING\n";				// Moving the robot to search the ball
+		cout << "SEARCHING\n";			// Moving the robot to search the ball
 		geometry_msgs::Twist vel;
-		vel.angular.z = 0.2 ;				// Giving a angular velocity around z axis
+		vel.angular.z = 0.2 ;			// Giving a angular velocity around z axis
 		if (controlVelocities(vel))
-			pub_robot.publish(vel);			// Publishing the velocity
+			pub_robot.publish(vel);		// Publishing the velocity
 	}
 	else {
 		found = true;
@@ -73,24 +73,24 @@ void alignment(float yaw, double robot_yaw) {
 
 	// Understand the fastest way to align
 	if ((robot_yaw > yaw + 0.05 || robot_yaw < yaw + M_PI - 0.05) && abs(misalignment) > 0.05) {
-		cout << "CLOCKWISE\n"; 										// Moving the robot clockwise
+		cout << "CLOCKWISE\n"; 						// Moving the robot clockwise
 		geometry_msgs::Twist vel;
 		vel.angular.z = -0.3 * misalignment / abs(misalignment);	// Giving an angular velocity on z axis
 		if (controlVelocities(vel))
-			pub_robot.publish(vel);									// Publishing the velocity
+			pub_robot.publish(vel);					// Publishing the velocity
 	}
 	else if ((robot_yaw > yaw + M_PI + 0.05 || robot_yaw < yaw - 0.05) && abs(misalignment) > 0.05) {
-		cout << "ANTICLOCKWISE\n";									// Moving the robot anti-clockwise
+		cout << "ANTICLOCKWISE\n";					// Moving the robot anti-clockwise
 		geometry_msgs::Twist vel;
 		vel.angular.z = +0.3 * misalignment / abs(misalignment);	// Giving an angular velocity on z axis
 		if (controlVelocities(vel))
-			pub_robot.publish(vel);									// Publishing the velocity
+			pub_robot.publish(vel);					// Publishing the velocity
 	}
 	else {
-		aligned = true;					// Now the robot is aligned on the ine ball-porta
+		aligned = true;				// Now the robot is aligned on the ine ball-porta
 		cout << "Aligned\n";
 		geometry_msgs::Twist vel;
-		vel.angular.z = 0;				// Giving a zero angular velocity on z axis
+		vel.angular.z = 0;			// Giving a zero angular velocity on z axis
 		if (controlVelocities(vel))
 			pub_robot.publish(vel);		// Publishing the velocity
 	}
@@ -100,22 +100,22 @@ void alignment(float yaw, double robot_yaw) {
 // Function to move the robot back if it is not behind the ball
 void moveBack() {
 
-	float p = -m * robot[0] + robot[1];						 // y coordinate at the origin of the line of the aligned robot
-	float distance_rette = abs(p - q) / sqrt(pow(m, 2) + 1); // Distance btw two parallel lines
-	float y_robot_new;										 // y coordinate that the robot will have after the translation
+	float p = -m * robot[0] + robot[1];				// y coordinate at the origin of the line of the aligned robot
+	float distance_rette = abs(p - q) / sqrt(pow(m, 2) + 1);	// Distance btw two parallel lines
+	float y_robot_new;						// y coordinate that the robot will have after the translation
 
 	// Case 1: Parallel vertical lines
 	if (abs(q) > 100) {
-		y_robot_new = robot[1];				// If the lines are vertical the y of the robot remains the same
+		y_robot_new = robot[1];			// If the lines are vertical the y of the robot remains the same
 		if (ball[1] < y_robot_new + 1) { 	// If the robot after the translation will not be behind the ball
 			cout << "MOVING BACK\n";        // Moving the robot back
 			geometry_msgs::Twist vel;
-			vel.linear.x = -0.2;			// Giving a linear velocity on x axis
+			vel.linear.x = -0.2;		// Giving a linear velocity on x axis
 			if (controlVelocities(vel))
-				pub_robot.publish(vel);		// Publishing the velocity
+				pub_robot.publish(vel);	// Publishing the velocity
 		}
 		else {
-			behindBall = true;				// The robot is now behind the ball
+			behindBall = true;		// The robot is now behind the ball
 			cout << "Behind the ball\n";
 		}
 	}
@@ -132,12 +132,12 @@ void moveBack() {
 		if (ball[1] < y_robot_new + 1) { 	// If the robot after the translation will not be behind the ball
 			cout << "MOVING BACK\n";        // Moving the robot back
 			geometry_msgs::Twist vel;
-			vel.linear.x = -0.2;			// Giving a linear velocity on x axis
+			vel.linear.x = -0.2;		// Giving a linear velocity on x axis
 			if (controlVelocities(vel))
-				pub_robot.publish(vel);		// Publishing the velocity
+				pub_robot.publish(vel);	// Publishing the velocity
 		}
 		else {
-			behindBall = true;				// The robot is now behind the ball
+			behindBall = true;		// The robot is now behind the ball
 			cout << "Behind the ball\n";
 		}
 	}
@@ -154,12 +154,12 @@ void moveBack() {
 		if (ball[1] < y_robot_new + 1) {	// If the robot after the translation will not be behind the ball
 			cout << "MOVING BACK\n";        // Moving the robot back
 			geometry_msgs::Twist vel;
-			vel.linear.x = -0.2;			// Giving a linear velocity on x axis
+			vel.linear.x = -0.2;		// Giving a linear velocity on x axis
 			if (controlVelocities(vel))
-				pub_robot.publish(vel);		// Publishing the velocity
+				pub_robot.publish(vel);	// Publishing the velocity
 		}
 		else {
-			behindBall = true;				// The robot is now behind the ball
+			behindBall = true;		// The robot is now behind the ball
 			cout << "Behind the ball\n";
 		}
 	}
@@ -169,31 +169,31 @@ void moveBack() {
 // Function to move the robot on the trajectory ball-porta
 void moveToTrajectory() {
 
-	float p = -m * robot[0] + robot[1];							// y coordinate at the origin of the line of the aligned robot
+	float p = -m * robot[0] + robot[1];				// y coordinate at the origin of the line of the aligned robot
 	float distance_rette = abs(p - q) / sqrt(pow(m, 2) + 1);	// Distance btw to parallel lines
-	float distance_rette_vertical = robot[0] - ball[0];			// Distance btw two parallel lines, vertical case
+	float distance_rette_vertical = robot[0] - ball[0];		// Distance btw two parallel lines, vertical case
 
 	
-	if (abs(q) > 100 && abs(distance_rette_vertical) > 0.1) { 							// Particular case of vertical lines
-		cout << "TRASLATION\n";															// Moving the robot on the trajectory ball-porta
+	if (abs(q) > 100 && abs(distance_rette_vertical) > 0.1) { 	// Particular case of vertical lines
+		cout << "TRASLATION\n";					// Moving the robot on the trajectory ball-porta
 		geometry_msgs::Twist vel;
 		vel.linear.y = 0.2 * distance_rette_vertical / abs(distance_rette_vertical); 	// Giving a linear velocity on y axis
 		if (controlVelocities(vel))
-			pub_robot.publish(vel);														// Publishing the velocity
+			pub_robot.publish(vel);				// Publishing the velocity
 
 	}
-	else if (abs(distance_rette) > 0.1) {	// Normal case of oblique lines
-		cout << "TRASLATION\n";				// Moving the robot on the trajectory ball-porta
+	else if (abs(distance_rette) > 0.1) {		// Normal case of oblique lines
+		cout << "TRASLATION\n";			// Moving the robot on the trajectory ball-porta
 		geometry_msgs::Twist vel;
 		vel.linear.y = 0.2 * sign_v;		// Giving a linear velocity on y axis
 		if (controlVelocities(vel))
-			pub_robot.publish(vel);			// Publishing the velocity
+			pub_robot.publish(vel);		// Publishing the velocity
 	}
 	else {
 		onTrajectory = true;			// The robot is now on the trajectory ball-porta
 		cout << "I've traslated\n";						
 		geometry_msgs::Twist vel;
-		vel.linear.y = 0;				// Giving a zero linear velocity on y axis
+		vel.linear.y = 0;			// Giving a zero linear velocity on y axis
 		if (controlVelocities(vel))
 			pub_robot.publish(vel);		// Publishing the velocity
 	}
@@ -205,7 +205,7 @@ void kick() {
 
 	cout << "GO FORWARD\n";				// Moving the robot foward to kick the ball
 	geometry_msgs::Twist vel;
-	vel.linear.x = 0.3;					// Giving a linear velocity on x axis
+	vel.linear.x = 0.3;				// Giving a linear velocity on x axis
 	if (controlVelocities(vel))
 		pub_robot.publish(vel);			// Publishing the velocity
 }
@@ -289,10 +289,10 @@ void ballDetectionCallback(const ass_2::Translation::ConstPtr &msg) {
 
 	// Tranforming ball position from camera frame to world frame
 	float T_r[4][4];	// Transformation matrix between world and robot
-	T_r[0][0] = cos(robot_yaw);				T_r[0][1] = cos(M_PI/2 - robot_yaw);	T_r[0][2] = cos(M_PI/2);		T_r[0][3] = robot[0];
-	T_r[1][0] = cos(robot_yaw + M_PI/2);	T_r[1][1] = cos(robot_pitch);			T_r[1][2] = cos(M_PI/2);		T_r[1][3] = robot[1];
-	T_r[2][0] = cos(M_PI/2);				T_r[2][1] = cos(M_PI/2);				T_r[2][2] = cos(robot_roll);	T_r[2][3] = robot[2];
-	T_r[3][0] = 0;							T_r[3][1] = 0;							T_r[3][2] = 0;					T_r[3][3] = 1;
+	T_r[0][0] = cos(robot_yaw);		T_r[0][1] = cos(M_PI/2 - robot_yaw);	T_r[0][2] = cos(M_PI/2);	T_r[0][3] = robot[0];
+	T_r[1][0] = cos(robot_yaw + M_PI/2);	T_r[1][1] = cos(robot_pitch);		T_r[1][2] = cos(M_PI/2);	T_r[1][3] = robot[1];
+	T_r[2][0] = cos(M_PI/2);		T_r[2][1] = cos(M_PI/2);		T_r[2][2] = cos(robot_roll);	T_r[2][3] = robot[2];
+	T_r[3][0] = 0;				T_r[3][1] = 0;				T_r[3][2] = 0;			T_r[3][3] = 1;
 
 	float T_b[4][4];	// Trasformation matrix between robot and ball
 	T_b[0][0] = 1;		T_b[0][1] = 0;		T_b[0][2] = 0;		T_b[0][3] = ball_camera[0];
@@ -318,9 +318,9 @@ void ballDetectionCallback(const ass_2::Translation::ConstPtr &msg) {
         }
     }
 
-	m = (porta[1] - ball[1]) / (porta[0] - ball[0]);						// Angular coefficent of the line ball-porta
+	m = (porta[1] - ball[1]) / (porta[0] - ball[0]);			// Angular coefficent of the line ball-porta
 	q = (porta[0] * ball[1] - ball[0] * porta[1]) / (porta[0] - ball[0]);	// y coordinates at the origin of the line ball-porta
-	yaw = atan(m);															// Yaw angle wrt x axis of the world frame
+	yaw = atan(m);								// Yaw angle wrt x axis of the world frame
 }
 
 
